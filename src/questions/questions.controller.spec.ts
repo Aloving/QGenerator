6274d06824193a1d5aacfd72c4c8ea5fd618003a
Alexey Kartushin <alexey.kartushin@onsolve.com>
2021-03-27 19:38:12 +0300

    Add tests for questions controller

--- src/questions/questions.controller.spec.ts
+++ src/questions/questions.controller.spec.ts
@@ -1,20 +1,76 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { QuestionsController } from './questions.controller';
 import { QuestionsService } from './questions.service';
+import { compileTestQuestionModule } from './helpers/testHelpers';
+import { CreateQuestionDto, UpdateQuestionDto } from './dto';
 
 describe('QuestionsController', () => {
-  let controller: QuestionsController;
+  let questionsController: QuestionsController;
+  let questionsService: QuestionsService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [QuestionsController],
-      providers: [QuestionsService],
-    }).compile();
+    const questionModule = await compileTestQuestionModule();
 
-    controller = module.get<QuestionsController>(QuestionsController);
+    questionsController = questionModule.questionController;
+    questionsService = questionModule.questionService;
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('should call create method and return value', async () => {
+      const inputData: CreateQuestionDto = {
+        likes: 0,
+        dislikes: 0,
+        text: 'test question',
+        answers: [],
+      };
+
+      jest.spyOn(questionsService, 'create').mockResolvedValue('ok' as never);
+
+      const response = await questionsController.create(inputData);
+
+      expect(questionsService.create).toHaveBeenCalledWith(inputData);
+      expect(response).toEqual('ok');
+    });
+  });
+
+  describe('findAll', () => {
+    it('should call findAll method and return value', async () => {
+      jest
+        .spyOn(questionsService, 'findAll')
+        .mockResolvedValue(['ok', 'ok'] as never);
+
+      const response = await questionsController.findAll();
+
+      expect(questionsService.findAll).toHaveBeenCalled();
+      expect(response).toEqual(['ok', 'ok']);
+    });
+  });
+
+  describe('update', () => {
+    it('should call update method and return value', async () => {
+      const inputData: UpdateQuestionDto = {
+        likes: 0,
+        dislikes: 0,
+        text: 'test question',
+        answers: [],
+      };
+
+      jest.spyOn(questionsService, 'update').mockResolvedValue('ok' as never);
+
+      const response = await questionsService.update(10, inputData);
+
+      expect(questionsService.update).toHaveBeenCalledWith(10, inputData);
+      expect(response).toEqual('ok');
+    });
+  });
+
+  describe('remove', () => {
+    it('should call remove method and return value', async () => {
+      jest.spyOn(questionsService, 'remove').mockResolvedValue('ok' as never);
+
+      const response = await questionsService.remove(10);
+
+      expect(questionsService.remove).toHaveBeenCalledWith(10);
+      expect(response).toEqual('ok');
+    });
   });
 });
