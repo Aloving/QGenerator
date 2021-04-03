20cce0a7b8af3928ef568ca8d6ba31076368685e
Alexey Kartushin <alexey.kartushin@onsolve.com>
2021-04-03 15:47:02 +0300

    BK-12 Add answer controller, add likes / dislikes to questions

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -37,12 +37,36 @@ export class QuestionsService implements IQuestionService {
     return await this.questionRepository.findOne();
   }
 
-  async generate(excludeIds) {
+  async generate(excludeIds: number[]) {
     const question = await this.findOne();
 
     return Promise.resolve({
       excludeIds,
       question,
+    });
+  }
+
+  async like(id: number) {
+    const {
+      id: _id,
+      ...questionToUpdate
+    } = await this.questionRepository.findOne(id);
+
+    return this.update(id, {
+      ...questionToUpdate,
+      likes: questionToUpdate.likes + 1,
+    });
+  }
+
+  async dislike(id: number) {
+    const {
+      id: _id,
+      ...questionToUpdate
+    } = await this.questionRepository.findOne(id);
+
+    return this.update(id, {
+      ...questionToUpdate,
+      dislikes: questionToUpdate.dislikes + 1,
     });
   }
 }
