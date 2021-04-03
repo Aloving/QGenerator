10bffabe1e8657e7c30686fc92f5a61b28177370
Alexey Kartushin <alexey.kartushin@onsolve.com>
2021-04-03 13:55:00 +0300

    BK-5 Describe api controllers

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -7,33 +7,59 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+
 import { QuestionsService } from './questions.service';
 import {
   UpdateQuestionDto,
   CreateQuestionDto,
   GetRandomQuestionDto,
+  GetRandomQuestionResponseDto,
 } from './dto';
+import { Question } from './entities';
 
+@ApiTags('questions')
 @Controller('questions')
 export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
 
   @Post('/create')
+  @ApiResponse({
+    status: 200,
+    type: Question,
+    description: 'A point to create question',
+  })
   create(@Body() createQuestionDto: CreateQuestionDto) {
     return this.questionsService.create(createQuestionDto);
   }
 
   @Post('/generate')
+  @ApiResponse({
+    status: 200,
+    type: GetRandomQuestionResponseDto,
+    description: 'Path to get random question, it supports id excluding',
+  })
   generateAnswer(@Body() getRandomQuestionDto: GetRandomQuestionDto) {
     return this.questionsService.generate(getRandomQuestionDto);
   }
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    type: Question,
+    isArray: true,
+    description: 'Point to get all questions',
+  })
   findAll() {
     return this.questionsService.findAll();
   }
 
   @Put(':id')
+  @ApiResponse({
+    status: 200,
+    type: Question,
+    description: 'Point to update an existing question',
+  })
   update(
     @Param('id') id: string,
     @Body() updateQuestionDto: UpdateQuestionDto,
@@ -42,6 +68,11 @@ export class QuestionsController {
   }
 
   @Delete('/:id')
+  @ApiResponse({
+    status: 200,
+    type: Boolean,
+    description: 'A point to delete question out of the app',
+  })
   remove(@Param('id') id: string) {
     return this.questionsService.remove(+id);
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { CreateUserDto } from './dto';
 import { UsersService } from './users.service';
+import { User } from './entities';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly questionsService: UsersService) {}
 
   @Post('/create')
+  @ApiResponse({
+    status: 200,
+    type: User,
+    description: 'A point to create a new user',
+  })
   async create(@Body() createUser: CreateUserDto) {
     return this.questionsService.create(createUser);
   }
